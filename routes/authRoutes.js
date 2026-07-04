ac9419e1ab63f7af37c236ce30fb02038e01dbf7
aaryananu2210 <aaryananu2210@gmail.com>
2026-07-04 13:24:53 +0530

    added validation middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const { signup ,login,getProfile} = require("../controllers/authController");
+const { body } = require("express-validator");
+const validate = require("../middleware/validationMiddleware");
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email"),
+
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters")
+  ],
+  validate,
+  signup
+);
 
 router.post("/login", login);
 
