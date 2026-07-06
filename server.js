e9ebed531dc527b577ad2942b6769caf17b8c104
aaryananu2210 <aaryananu2210@gmail.com>
2026-07-06 17:16:30 +0530

    Configure CORS for frontend

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const postRoutes = require("./routes/postRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors({
+    origin: "https://github-dashboard-sand.vercel.app/"
+}));
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
